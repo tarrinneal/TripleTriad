c2737cf51a885ef1e8d12a1c415325a700459c36
tarrinneal <tarrinneal@gmail.com>
2021-03-30 13:50:56 -0700

    fixed card backgrounds

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import cards from "../cardStats.js"
 
-const Card = ({loc, card}) => {
+const Card = ({loc, card, player = 1}) => {
   let row = Math.ceil((9 - loc + 1) / 3)
   if (row === 1) {
     row = 3
@@ -9,8 +9,9 @@ const Card = ({loc, card}) => {
     row = 1
   }
     return (
-      <div className={`card ${cards[card - 1].id}`} >
-       <img className={'cardImg'} src={`./img/cards/${card.toString().padStart(3, '0')}.png`} />
+      <div className={`card ${cards[card - 1].id}`}>
+        <img className={'cardBackground'} src={`./img/card${player}.png`} />
+        <img className={'cardImg'} src={`./img/cards/${card.toString().padStart(3, '0')}.png`} />
       </div>
     )
 }
